97dfaea8594376e18adcd26535539a723ec43f62
Mathankumar Natesan <mathankumar.n@idp.com>
2026-08-09 11:29:02 +0530

    fix(inventory-service): export Express router cleanly in app.js

--- services/inventory-service/src/app.js
+++ services/inventory-service/src/app.js
@@ -1,8 +1,9 @@
 const createServiceApp = require('@freshmart/service-shared').createServiceApp;
-const routes = require('./routes');
+const routes = require('./routes/index.js');
 
 module.exports = createServiceApp({
   mountRoutes(app) {
+    app.use('/', routes);
     app.use('/inventory', routes);
     app.use('/v1/inventory', routes);
   },
